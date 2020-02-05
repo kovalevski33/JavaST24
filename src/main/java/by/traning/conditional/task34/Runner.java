fba1693d2ebb2b832e8af70c1d9d683a71be6a2c
Kovalevski <kovalevski6896033@gmail.com>
2020-02-05 21:57:55 +0300

    homework

---
+++ src/main/java/by/traning/conditional/task34/Runner.java
@@ -0,0 +1,32 @@
+package by.traning.conditional.task34;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Runner {
+     /*Составить программу, реализующую эпизод применения компьютера в книжном магазине. Компьютер запрашивает стоимость книг, сумму денег, внесенную покупателем,
+     если сдачи не требуется, печатает на экране «спасибо»,
+     если денег внесено больше, чем необходимо, то печатает «возьмите сдачу» и указывает сумму сдачи; если денег недостаточно, то печатает сообщение об этом и указывает размер недостающей суммы.*/
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        System.out.println("Введите стоимость книг");
+        int a = Integer.parseInt(reader.readLine());
+        System.out.println("Ввведите сумму денег, внесенную покупателем");
+        int b = Integer.parseInt(reader.readLine());
+        int c = b - a;
+
+        if (c == 0){
+            System.out.println("Спасибо");
+        }
+        if (c > 0){
+            System.out.println("Возьмите сдачу " + c);
+        }
+        if (c < 0){
+            System.out.println("Денег недостаточно " + c);
+        }
+    }
+
+}
--- src/main/java/by/traning/conditional/task8/Runner.java
+++ src/main/java/by/traning/conditional/task8/Runner.java
@@ -5,28 +5,14 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Runner {
-     /*Составить программу, реализующую эпизод применения компьютера в книжном магазине. Компьютер запрашивает стоимость книг, сумму денег, внесенную покупателем,
-     если сдачи не требуется, печатает на экране «спасибо»,
-     если денег внесено больше, чем необходимо, то печатает «возьмите сдачу» и указывает сумму сдачи; если денег недостаточно, то печатает сообщение об этом и указывает размер недостающей суммы.*/
+    //Составить программу нахождения наименьшего из квадратов двух чисел а и b.
 
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
-        System.out.println("Введите стоимость книг");
         int a = Integer.parseInt(reader.readLine());
-        System.out.println("Ввведите сумму денег, внесенную покупателем");
         int b = Integer.parseInt(reader.readLine());
-        int c = b-a;
 
-        if (c==0){
-            System.out.println("Спасибо");
-        }
-        if (c>0){
-            System.out.println("Возьмите сдачу " + c);
-        }
-        if (c<0){
-            System.out.println("Денег недостаточно " + c);
-        }
+        System.out.println("Наименьшее из квадратов двух чисел: " + Math.min(a*a,b*b));
     }
-
 }
